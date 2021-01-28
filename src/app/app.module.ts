33c70b0e70fc357f0d517b35267d5c4fbfd8df10
Cody Packer <cody.packer@valpo.edu>
2021-01-28 00:38:30 -0600

    Initial website, including 2 pages/views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
-  ],
+  BrowserModule,
+  RouterModule.forRoot([
+    {path: 'home', component: HomeComponent},
+    {path: 'about-us', component: AboutUsComponent},
+  ]),
+],
   providers: [],
   bootstrap: [AppComponent]
 })
