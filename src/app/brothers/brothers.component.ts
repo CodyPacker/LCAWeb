1f856e3e60a2c314543f3e1d9f72bb1c70c28916
Cody Packer <cody.packer@valpo.edu>
2021-01-28 11:15:32 -0600

    More Pages, slight design on home page and index page

---
+++ src/app/brothers/brothers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brothers',
+  templateUrl: './brothers.component.html',
+  styleUrls: ['./brothers.component.css']
+})
+export class BrothersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
