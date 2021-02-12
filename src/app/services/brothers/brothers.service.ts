5813133ec8f8dbfcd279a072c2c3fc801e63e53d
Terry <terry.wade@valpo.edu>
2021-02-12 11:53:37 -0600

    Add brother model and service

---
+++ src/app/services/brothers/brothers.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrothersService {
+
+  constructor() { }
+}
